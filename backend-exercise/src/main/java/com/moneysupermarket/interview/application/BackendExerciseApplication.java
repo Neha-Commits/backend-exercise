efe8ea230c55c754eeddcea5ec6f69c1486daa58
NehaLalwani23 <nehalalwani73@yahoo.com>
2020-11-15 18:06:55 +0530

    Added Files

---
+++ backend-exercise/src/main/java/com/moneysupermarket/interview/application/BackendExerciseApplication.java
@@ -0,0 +1,11 @@
+package com.moneysupermarket.interview.application;
+
+@SpringBootApplication
+public class BackendExerciseApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(BackendExcerciseApplication.class, args);
+
+
+    }
+}
