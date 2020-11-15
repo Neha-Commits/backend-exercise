b0d981f21d7abf4f7ae954e6514df576b21c1c4a
NehaLalwani23 <nehalalwani73@yahoo.com>
2020-11-15 18:46:53 +0530

    Added some code

--- backend-exercise/src/main/java/com/moneysupermarket/interview/controller/BackendExerciseController.java
+++ backend-exercise/src/main/java/com/moneysupermarket/interview/controller/BackendExerciseController.java
@@ -1,5 +1,6 @@
 package com.moneysupermarket.interview.controller;
 
+import com.moneysupermarket.interview.service.ProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class BackendExerciseController {
 
     @Autowired
+    ProcessingService processService;
 
 
 
     @GetMapping("/exchange")
     public ResponseEntity<String> getExchangeDetails() {
-        //service.getDetails();
+        processService.getExchangeValues();
 
         return new ResponseEntity<String>("OK", HttpStatus.OK);
     }
